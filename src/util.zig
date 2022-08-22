1b865262c5a39432a26c3cb99ddd383741b3c597
Sosuke Hosokawa <so298go@gmail.com>
2022-08-22 16:12:32 +0900

    modify VariableLengthInt

--- src/util.zig
+++ src/util.zig
@@ -14,14 +14,14 @@ test "getBigEndian" {
     try testing.expect(getBigEndianInt(u32, 0x01020304) == @as(u32, 0x04030201));
 }
 
-pub const WriteError = error {
-    BufferShortError,
+pub const WriteError = error{
+    BufferTooShort,
 };
 
 /// write value in BIG ENDIAN and returns written byte count
 pub fn writeIntReturnSize(comptime T: type, dst: []u8, value: T) WriteError!usize {
     const write_size = @as(usize, @bitSizeOf(T) / 8);
-    if (dst.len < write_size) return WriteError.BufferShortError;
+    if (dst.len < write_size) return WriteError.BufferTooShort;
 
     std.mem.writeInt(T, dst[0..write_size], value, .Big);
     return write_size;
@@ -29,97 +29,122 @@ pub fn writeIntReturnSize(comptime T: type, dst: []u8, value: T) WriteError!usiz
 
 /// copy buffer with offset and returns written byte count
 pub fn copyReturnSize(dst: []u8, source: []const u8) WriteError!usize {
-    if (dst.len < source.len) return WriteError.BufferShortError;
+    if (dst.len < source.len) return WriteError.BufferTooShort;
 
     const len = source.len;
     std.mem.copy(u8, dst[0..len], source);
     return len;
 }
 
+/// implementation for Variable-Length Interger
+/// https://www.rfc-editor.org/rfc/rfc9000.html#name-variable-length-integer-enc
 pub const VariableLengthInt = struct {
     len: usize,
     value: u64,
 
     const Self = @This();
 
-    const parseError = error{
-        ShortLenError,
-        InvalidLenError,
-    };
+    pub const Error = error{ VLIntLengthShort, VLIntInvalidLength, VLIntInvalidValue } || WriteError;
 
-    /// convert self to integer type T as variable length format
-    pub fn toInt(self: *const Self, comptime T: type) parseError!T {
-        const len_bits: T = switch (T) {
-            u8 => 0x0000000000000000,
-            u16 => 0x0000000000004000,
-            u32 => 0x0000000080000000,
-            u64 => 0xf000000000000000,
-            else => @compileError("T must be u8, u16, u32 or u64"),
+    /// encode to u8 array
+    pub fn encode(self: *const Self, out: []u8) Error!usize {
+        if (out.len < self.len) return Error.BufferTooShort;
+        if (self.value >= (@as(u64, 1) << @intCast(u6, self.len * 8 - 2)))
+            return Error.VLIntLengthShort; // check for having enough length to express the value
+
+        const len_bits_mask: u8 = switch (self.len) {
+            1 => 0x00,
+            2 => 0x40,
+            4 => 0x80,
+            8 => 0xC0,
+            else => return Error.VLIntInvalidLength,
         };
 
-        const mask: T = switch (T) {
-            u8 => 0x000000000000003f,
-            u16 => 0x0000000000003fff,
-            u32 => 0x000000003fffffff,
-            u64 => 0x3fffffffffffffff,
-            else => @compileError("T must be u8, u16, u32 or u64"),
+        const count = switch (self.len) {
+            1 => writeIntReturnSize(u8, out[0..1], @intCast(u8, self.value)),
+            2 => writeIntReturnSize(u16, out[0..2], @intCast(u16, self.value)),
+            4 => writeIntReturnSize(u32, out[0..4], @intCast(u32, self.value)),
+            8 => writeIntReturnSize(u64, out[0..8], @intCast(u64, self.value)),
+            else => unreachable,
         };
 
-        const masked: T = @intCast(T, self.value & mask);
-        return masked | len_bits;
+        out[0] |= len_bits_mask;
+
+        return count;
     }
 
-    /// create variableLenInt from u8 array
-    /// array length must be longer than passed len, 
-    /// which must equal to 1, 2, 4 or 8 
-    pub fn fromU8Array(len: usize, array: []const u8) parseError!Self {
-        if (array.len < len) {
-            return parseError.ShortLenError;
-        }
+    /// decode variable-length-interger-coded u8 array
+    pub fn decode(array: []const u8) Error!Self {
+        if (array.len == 0) return Error.BufferTooShort;
+        const len_bits = array[0] & 0xC0;
+        const length: usize = switch (len_bits) {
+            0x00 => 1,
+            0x40 => 2,
+            0x80 => 4,
+            0xC0 => 8,
+            else => unreachable,
+        };
 
-        var copy_arr: [8]u8 = undefined;
+        if (array.len < length) return Error.BufferTooShort;
 
-        std.mem.copy(u8, copy_arr[0..len], array[0..len]);
-        copy_arr[0] &= 0x3f;
+        var copy: [8]u8 = undefined;
+        std.mem.copy(u8, &copy, array[0..length]);
+        copy[0] &= 0x3F; // remove length field
 
-        const value: u64 = switch (len) {
-            1 => std.mem.readInt(u8, copy_arr[0..1], .Big),
-            2 => std.mem.readInt(u16, copy_arr[0..2], .Big),
-            4 => std.mem.readInt(u32, copy_arr[0..4], .Big),
-            8 => std.mem.readInt(u64, copy_arr[0..8], .Big),
-            else => return parseError.InvalidLenError,
+        const value = switch (length) {
+            1 => @intCast(usize, std.mem.readIntBig(u8, copy[0..1])),
+            2 => @intCast(usize, std.mem.readIntBig(u16, copy[0..2])),
+            4 => @intCast(usize, std.mem.readIntBig(u32, copy[0..4])),
+            8 => @intCast(usize, std.mem.readIntBig(u64, copy[0..8])),
+            else => unreachable,
+        };
+
+        return Self{
+            .len = length,
+            .value = value,
+        };
+    }
+
+    pub fn fromU64(value: u64) Error!Self {
+        const ONE_BIT_MAX = (1 << 6) - 1;
+        const TWO_BITS_MAX = (1 << 14) - 1;
+        const FOUR_BITS_MAX = (1 << 30) - 1;
+        const EIGHT_BITS_MAX = (1 << 62) - 1;
+
+        const length: usize = switch (value) {
+            0...ONE_BIT_MAX => 1,
+            (ONE_BIT_MAX + 1)...TWO_BITS_MAX => 2,
+            (TWO_BITS_MAX + 1)...FOUR_BITS_MAX => 4,
+            (FOUR_BITS_MAX + 1)...EIGHT_BITS_MAX => 8,
+            else => return Error.VLIntInvalidValue,
         };
 
         return Self{
             .value = value,
-            .len = len,
-        };
-    }
-
-    /// calculate length from the first byte of variable length interger
-    pub fn calcLenBits(byte: u8) usize {
-        const mask: u8 = 0xc0;
-        const masked: u8 = byte & mask;
-        return switch (masked) {
-            0x00 => 1,
-            0x40 => 2,
-            0x80 => 4,
-            0xc0 => 8,
-            else => unreachable,
+            .len = length,
         };
     }
 };
 
-test "convert u8 array to variable length int" {
+test "decode u8 array to variable length int" {
     const array1: [4]u8 = .{ 0x81, 0x04, 0x48, 0xad };
-    const len1 = VariableLengthInt.calcLenBits(array1[0]);
-    const v_int1 = try VariableLengthInt.fromU8Array(len1, array1[0..len1]);
-    try testing.expectEqual(@as(usize, 4), len1);
+    const v_int1 = try VariableLengthInt.decode(&array1);
+    try testing.expectEqual(@as(usize, 4), v_int1.len);
     try testing.expectEqual(@as(u64, 0x010448ad), v_int1.value);
 }
 
-test "convert variable length int to normal integer" {
-    const v_int = VariableLengthInt{ .value = 0x36f8, .len = 2 };
-    const converted = try v_int.toInt(u16);
-    try testing.expectEqual(@as(u16, 0x36f8 | 0x4000), converted);
+test "encode variable length int to u8 array" {
+    const v_int = VariableLengthInt{ .value = 0x010448ad, .len = 4 };
+    var encoded: [8]u8 = undefined;
+    const encode_len = try v_int.encode(&encoded);
+    try testing.expectEqual(@as(usize, 4), encode_len);
+    try testing.expectEqualSlices(u8, &[_]u8{ 0x81, 0x04, 0x48, 0xad }, encoded[0..v_int.len]);
+}
+
+test "convert to variable length int from u64" {
+    const v_int = try VariableLengthInt.fromU64(0x010448ad);
+    try testing.expectEqual(
+        VariableLengthInt{ .value = 0x010448ad, .len = 4 },
+        v_int,
+    );
 }
