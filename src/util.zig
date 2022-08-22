aae546a6dd1728308ceb8f8cc95af74296246b76
Sosuke Hosokawa <so298go@gmail.com>
2022-08-22 16:13:17 +0900

    remove unused function

--- src/util.zig
+++ src/util.zig
@@ -2,18 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 const testing = std.testing;
 
-pub fn getBigEndianInt(comptime T: type, int: T) T {
-    const ret: T = switch (std.Target.Cpu.Arch.endian(builtin.cpu.arch)) {
-        .Big => int,
-        .Little => @byteSwap(T, int),
-    };
-    return ret;
-}
-
-test "getBigEndian" {
-    try testing.expect(getBigEndianInt(u32, 0x01020304) == @as(u32, 0x04030201));
-}
-
 pub const WriteError = error{
     BufferTooShort,
 };
