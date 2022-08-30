c1fb631138917ce82d6bc2aa9a0233457325b087
Sosuke Hosokawa <so298go@gmail.com>
2022-08-30 21:53:06 +0900

    modify test udp server

--- test_tools/udp_server_py/udp_server.py
+++ test_tools/udp_server_py/udp_server.py
@@ -3,15 +3,28 @@ this is a UDP server to test src/udp.zig
 '''
 import socket
 from sys import stderr
+import logging
 
 HOST = ''
 PORT = 4000
 BUFFER_SIZE = 1024
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
-    server.bind((HOST, PORT))
-    msg, address = server.recvfrom(BUFFER_SIZE)
-    if msg == b'send test':
-        server.sendto(b'recieve test', address)
-    else:
-        print("test failed", file=stderr)
+logger = logging.getLogger("udp test server")
+logging.basicConfig(
+    format="%(asctime)s %(levelname)s %(name)s %(message)s",
+    level=logging.DEBUG
+)
+
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
+        server.bind((HOST, PORT))
+        logger.debug("server started")
+        while True:
+            msg, address = server.recvfrom(BUFFER_SIZE)
+            logger.debug(f'recieved message from {address}, "{msg.decode()}"')
+            if msg == b'send test':
+                server.sendto(b'recieve test', address)
+            else:
+                logger.debug("test failed")
+except KeyboardInterrupt:
+    pass
